298980189dc0cbdd55ede06be20e4baf977a6dac
Nimira43 <n.rabone72@aol.com>
2024-12-23 23:16:27 +0000

    JS written for the Eirene Productivity project brought in - needs to be modified for this project

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.querySelector('.navbar__mobile-menu-toggle')
+  const mobileMenu = document.querySelector('.navbar__mobile-menu-items')
+  toggleBtn.addEventListener('click', () => mobileMenu.classList.toggle('active'))
+})
+
+window.addEventListener('scroll', function() {
+  const navbar = this.document.querySelector('.navbar')
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar--scroll')
+  } else {
+    navbar.classList.remove('navbar--scroll')
+  }
+})
